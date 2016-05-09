4665d96ca3ad9f7d8d78ba5f664490d71f8c5219
Shruti Marwaha <shruti.marwaha@gmail.com>
2016-05-09 13:12:26 -0700

    added comment

--- KidneyDiseasePrediction/KidneyDiseasePredictor_SVM/app/helpers.R
+++ KidneyDiseasePrediction/KidneyDiseasePredictor_SVM/app/helpers.R
@@ -1,6 +1,8 @@
-# function to convert users input from app to matrix that can be fed to machine learning model
+# Features selected to be included in smaller svm model for the app. 
+# These are features which had variable importance of greater than 50 in the main svm model.
 svm_imp_features <- c("rbc.c","bu","sod","pc.abnormal","dm.no","sg.1.010","sg.1.020","sg.1.025","age","pe.no","bp")
 
+# Function to convert users input from app to matrix that can be fed to machine learning model
 create_input_matrix <- function(rbc_count,bu,sod,pc,dm,age,sg,pe,bp) {
   input_df <- data.frame(matrix(data=NA,nrow=1,ncol=length(svm_imp_features),dimnames=list(c(),svm_imp_features)))
   
